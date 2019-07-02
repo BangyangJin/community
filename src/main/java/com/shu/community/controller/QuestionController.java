3850cf28bc7b77bc670b806b5d5b9eed10662c06
BangyangJin <17826859481@163.com>
2019-07-02 10:22:10 +0800

    question detail

---
+++ src/main/java/com/shu/community/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package com.shu.community.controller;
+
+import com.shu.community.dto.QuestionDTO;
+import com.shu.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model){
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question",questionDTO);
+        return "question";
+
+    }
+
+}
