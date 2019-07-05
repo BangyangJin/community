0a4c6f2a5c4a69b18e498aa12ac70633b05fcc7f
BangyangJin <17826859481@163.com>
2019-07-05 14:06:13 +0800

    mybatis generator

--- src/main/java/com/shu/community/CommunityApplication.java
+++ src/main/java/com/shu/community/CommunityApplication.java
@@ -1,9 +1,11 @@
 package com.shu.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.shu.community.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/shu/community/interceptor/SessionInterceptor.java
+++ src/main/java/com/shu/community/interceptor/SessionInterceptor.java
@@ -2,6 +2,7 @@ package com.shu.community.interceptor;
 
 import com.shu.community.mapper.UserMapper;
 import com.shu.community.model.User;
+import com.shu.community.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Service
 public class SessionInterceptor implements HandlerInterceptor {
@@ -22,9 +24,11 @@ public class SessionInterceptor implements HandlerInterceptor {
             for(Cookie cookie : cookies){
                 if(cookie.getName().equals("token02")){
                     String token = cookie.getValue();
-                    User user = userMapper.findUserByToken(token);
-                    if(user != null){
-                        request.getSession().setAttribute("githubUser",user);
+                    UserExample userExample = new UserExample();
+                    userExample.createCriteria().andTokenEqualTo(token);
+                    List<User> users = userMapper.selectByExample(userExample);
+                    if(users.size() != 0){
+                        request.getSession().setAttribute("githubUser",users.get(0));
                     }
                     break;
                 }
